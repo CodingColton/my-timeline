8a694c0675e396c84f4944b28895d3931e9fb876
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-09-09 13:31:34 -0500

    Add basic color blocking functionality.

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const dateDictionary = {
-    "01": "Jan",
-    "02": "Feb",
-    "03": "Mar",
-    "04": "Apr",
-    "05": "May",
-    "06": "Jun",
-    "07": "Jul",
-    "08": "Aug",
-    "09": "Sep",
+    "1": "Jan",
+    "2": "Feb",
+    "3": "Mar",
+    "4": "Apr",
+    "5": "May",
+    "6": "Jun",
+    "7": "Jul",
+    "8": "Aug",
+    "9": "Sep",
     "10": "Oct",
     "11": "Nov",
     "12": "Dec"
 }
 
+let statementsProvided = ["1", "3", "5", "6", "10"];
+
+for (let i = 0; i < statementsProvided.length; i++) {
+    if (statementsProvided.length == 12) {
+        document.getElementById("year").style.color = "green";
+        break;
+    }
+    let currentItem = statementsProvided[i]
+    let month = dateDictionary[currentItem];
+    console.log(month);
+    document.getElementById(month).style.color = "blue";
+}
